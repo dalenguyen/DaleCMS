dcaaee003c7e4503fac601da01189a1ad1aeee39
Dale Nguyen <dungnq@itbox4vn.com>
2017-09-05 00:08:48 +0000

    Fixed intro button layout on homepage

--- resources/views/home/includes/intro.blade.php
+++ resources/views/home/includes/intro.blade.php
@@ -7,8 +7,8 @@
       <h2 class="alt">My name is Dale Nguyen. I am a Toronto based digital architect. My competent professional work environments are Laravel PHP Framework, MySQL, Javasript, HTML 5 and CSS and Lamp Stack.</h2>
     </header>
 
-    <footer>
+    <div>
       <a href="#portfolio" class="button scrolly"><span class="icon fa-arrow-circle-down"> Let's meet</span></a>
-    </footer>
+    </div>
   </div>
 </section>
